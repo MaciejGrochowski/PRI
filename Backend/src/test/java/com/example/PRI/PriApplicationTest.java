171c3ad74c4578b980cf9574c0f5ba60d4879f29
Zofia Dobrowolska <zofdob@st.amu.edu.pl>
2020-04-11 17:59:24 +0200

    Test spring :D

---
+++ Backend/src/test/java/com/example/PRI/PriApplicationTest.java
@@ -0,0 +1,17 @@
+package com.example.PRI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PriApplicationTest {
+
+        @Test
+        public void contextLoads() {
+    }
+}

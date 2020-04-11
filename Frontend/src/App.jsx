41e653b89bec248654584c43339479e0c06e5e1b
MaciejGrochowski <macgro1@st.amu.edu.pl>
2020-04-11 02:45:56 +0200

    Frontend - alpha of Table

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from "./Table";
+
+const columnConfig = () => (
+    [
+
+        {
+            title: 'Imię', field: 'name',
+            removable: true,
+            cellStyle: {
+                 backgroundColor: '#039be5',
+                color: 'green'
+            },
+            headerStyle: {
+                backgroundColor: 'blue',
+            }
+        },
+        { title: 'Nazwisko', field: 'surname', removable: true },
+        { title: 'Płeć', field: 'sex', lookup: {1: "M", 2: "K"} },
+        { title: 'Rasa', field: 'race' },
+        { title: 'Profesja', field: 'career'},
+        { title: 'Miejsce pobytu', field: 'livePlace'}
+    ]
+)
+
+
 
 class App extends React.Component{
 
-  render(){
-    const helloWorld = "Hello World. Chomiki są super :)";
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    componentDidMount() {
+        const data = [
+            { name: 'Johann', surname: 'Schmidt', sex: 1, race: "Człowiek", career: "Rzemieślnik", livePlace: "Altdorf" },
+            { name: 'Gertruda', surname: 'Schmidt', sex: 2, race: "Człowiek", career: "Rybak", livePlace: "Altdorf" }
+        ]
+        this.setState({data: data})
+    }
+
+    render(){
     return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              {helloWorld}
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+            <header className="App-header">
+            <Table
+            title={"Lista postaci"}
+            columnsConfig={columnConfig()}
+            data={this.state.data}
+            />
+            </header>
         </div>
     )
 
---
+++ Frontend/src/Table.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import MaterialTable from "material-table";
+import styled from 'styled-components';
+import TablePagination from "@material-ui/core/TablePagination";
+
+const Title = styled.div`
+    background-color: red
+    `
+
+
+class Table extends React.Component {
+
+    constructor() {
+        super();
+        this.state={
+            selectedRow: null
+        }
+    }
+
+
+    render(){
+        const {title, columnsConfig=[], data=[]} = this.props;
+        const divStyle = {
+            backgroundColor: 'blue'
+        };
+        return(
+            <div>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+            <MaterialTable
+                title={<Title>{title}</Title>}
+                onRowClick={((evt, selectedRow) => this.setState({ selectedRow }))}
+                style={divStyle}
+                components={{
+                    Row: props => (<div>{console.log(props)}<span>{props.data.name}</span><span>{props.data.surname}</span></div>),
+                    Pagination: props => (
+                        <TablePagination
+                            {...props}
+                            labelRowsPerPage={<div style={{fontSize: 22}}>{props.labelRowsPerPage}</div>}
+                            labelDisplayedRows={row => <div
+                                style={{fontSize: 30}}>{props.labelDisplayedRows(row)}</div>}
+                            SelectProps={{
+                                style: {
+                                    fontSize: 22
+                                }
+                            }}/>)
+                }}
+
+                columns={columnsConfig}
+                data={data}
+
+                options={{
+                    toolbar: false,
+                    search: true,
+                    paging: true,
+                    // filtering: true,
+                    headerStyle: {
+                        backgroundColor: 'black',
+                        color: 'red',
+                        marginTop: '-5px'
+                    },
+                    rowStyle: rowData => ({
+                        margin: '0 !important',
+                        padding: '0 !important',
+                        backgroundColor: (this.state.selectedRow && this.state.selectedRow.tableData.id === rowData.tableData.id) ? 'blue' : 'brown'})
+                }}
+            />
+            </div>
+
+
+        )
+
+
+
+    }
+
+
+}
+
+
+export default Table;

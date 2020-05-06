a9248b132e38e13f61096d8272dc895a0951aa11
MaciejGrochowski <macgro1@st.amu.edu.pl>
2020-05-07 01:29:55 +0200

    turn of cors

--- Backend/src/main/java/com/example/PRI/CorsConfiguration.java
+++ Backend/src/main/java/com/example/PRI/CorsConfiguration.java
@@ -16,7 +16,7 @@ public class CorsConfiguration
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedMethods("*");
             }
         };
     }

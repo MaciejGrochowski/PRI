a6eec111ff60850a97c695ad42fc4893300b4d1f
Zofia Dobrowolska <zofdob@st.amu.edu.pl>
2020-05-06 21:57:03 +0200

    Make war file great again

--- Backend/src/main/java/com/example/PRI/PriApplication.java
+++ Backend/src/main/java/com/example/PRI/PriApplication.java
@@ -3,11 +3,12 @@ package com.example.PRI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.lang.*;
 
 @SpringBootApplication
-public class PriApplication {
+public class PriApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PriApplication.class, args);

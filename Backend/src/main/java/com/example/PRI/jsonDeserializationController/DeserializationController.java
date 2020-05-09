d99df82d53055ac828d9f13907f85f478d59ec1c
Zofia Dobrowolska <zofdob@st.amu.edu.pl>
2020-05-09 16:31:03 +0200

    Surname deserialization function

--- Backend/src/main/java/com/example/PRI/jsonDeserializationController/DeserializationController.java
+++ Backend/src/main/java/com/example/PRI/jsonDeserializationController/DeserializationController.java
@@ -1,12 +1,16 @@
 package com.example.PRI.jsonDeserializationController;
 
 import com.example.PRI.entities.character.Name;
+import com.example.PRI.entities.character.Surname;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.example.PRI.services.character.NameService;
+import com.example.PRI.services.character.SurnameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -18,16 +22,33 @@ public class DeserializationController {
     @Autowired
     NameService nameService;
 
-    @RequestMapping("/json")
+    @Autowired
+    SurnameService surnameService;
+
+    @RequestMapping("/json/name")
     public void nameDeserializationAndDatabaseUpdate() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         String path = "src/jsons/name.json";
         String contents = Files.readString(Paths.get(path));
-        List<Name> listCar = objectMapper.readValue(contents, new TypeReference<List<Name>>(){});
-        for (Name name : listCar) {
+        List<Name> listName = objectMapper.readValue(contents, new TypeReference<List<Name>>() {
+        });
+        for (Name name : listName) {
             nameService.save(name);
         }
+    }
+
+    @RequestMapping("/json/surname")
+    public void surnameDeserializationAndDatabaseUpdate() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String path = "src/jsons/surname.json";
+        String contents = Files.readString(Paths.get(path));
+        List<Surname> listSurname = objectMapper.readValue(contents, new TypeReference<List<Surname>>() {
+        });
+        for (Surname surName : listSurname) {
+            surnameService.save(surName);
         }
     }
+}
 

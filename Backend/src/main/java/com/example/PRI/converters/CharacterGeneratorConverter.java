48a7818a934c2ddb6d57ec3ef8164d258fd71be3
Zofia Dobrowolska <zofdob@st.amu.edu.pl>
2020-05-19 19:50:16 +0200

    Surname and prediction conversion function

--- Backend/src/main/java/com/example/PRI/converters/CharacterGeneratorConverter.java
+++ Backend/src/main/java/com/example/PRI/converters/CharacterGeneratorConverter.java
@@ -3,15 +3,13 @@ package com.example.PRI.converters;
 import com.example.PRI.dtos.characters.CharacterInputDto;
 
 import com.example.PRI.entities.character.Character;
+import com.example.PRI.services.character.SurnameService;
 
 public class CharacterGeneratorConverter {
 
-    public static Character convert(CharacterInputDto inputDto){
+    public static Character convert(CharacterInputDto inputDto, SurnameService surnameService){
 
         Character output = new Character();
-//        output.setSurname(new Surname(inputDto.getSurname()));
-
-
         return output; //ToDo zadanie dla Zosi - konwersja
     }
 
--- Backend/src/main/java/com/example/PRI/services/character/generator/CharacterGenerator.java
+++ Backend/src/main/java/com/example/PRI/services/character/generator/CharacterGenerator.java
@@ -1,14 +1,20 @@
 package com.example.PRI.services.character.generator;
 
-import com.example.PRI.converters.CharacterGeneratorConverter;
 import com.example.PRI.dtos.characters.CharacterInputDto;
-import com.example.PRI.enums.Race;
+import com.example.PRI.entities.character.Name;
+import com.example.PRI.entities.character.Prediction;
+import com.example.PRI.entities.character.Surname;
 import com.example.PRI.services.GeneralService;
+import com.example.PRI.services.character.NameService;
+import com.example.PRI.services.character.PredictionService;
+import com.example.PRI.services.character.SurnameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.PRI.entities.character.Character;
 
+import javax.xml.stream.events.Namespace;
 import java.util.HashMap;
+import java.util.Optional;
 
 @Service
 public class CharacterGenerator extends GeneralService {
@@ -18,6 +24,15 @@ public class CharacterGenerator extends GeneralService {
 
     @Autowired
     private SurnameGenerator surnameGenerator;
+
+    @Autowired
+    SurnameService surnameService;
+
+    @Autowired
+    NameService nameService;
+
+    @Autowired
+    PredictionService predictionService;
 
     public Character generateFullCharacter(){
 
@@ -38,10 +53,52 @@ public class CharacterGenerator extends GeneralService {
     }
 
 
-    public Integer save(CharacterInputDto characterInputDto) {
-        Character character = CharacterGeneratorConverter.convert(characterInputDto);
+    public long save(CharacterInputDto characterInputDto) {
+        Character character = new Character();
+        //nameConvert(characterInputDto.getName(),character);
+        surnameConvert(characterInputDto.getSurname(),character);
+        predictionConvert(characterInputDto.getPrediction(),character);
 
 
-        return 0;
+        return character.getId();
+    }
+
+    public Optional<Surname> surnameConvert(String surNew, Character character){
+        Optional<Surname> surname = surnameService.findBySurname(surNew);
+        surname.ifPresent(character::setSurname);
+        if (!surname.isPresent() && surNew != null){
+            Surname surnameNew = new Surname();
+            surnameNew.setSurname(surNew);
+            surnameService.save(surnameNew);
+            character.setSurname(surnameNew);
+        }
+        return surname;
+    }
+
+   /* public Optional<Name> nameConvert(String inputName, Character character){
+        Optional<Name> nameOptional = nameService.findByName(inputName);
+        nameOptional.ifPresent(character::setName);
+        if (!nameOptional.isPresent() && inputName != null){
+            Name nameNew = new Name();
+            nameNew.setName(inputName);
+            nameService.save(nameNew);
+            character.setName(nameNew);
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+        return nameOptional;
+    }*/
+
+    public Optional<Prediction> predictionConvert(String inputPrediction, Character character){
+        Optional<Prediction> prediction = predictionService.findByText(inputPrediction);
+        prediction.ifPresent(character::setPrediction);
+        if (!prediction.isPresent() && inputPrediction != null){
+            Prediction predictionNew = new Prediction();
+            predictionNew.setText(inputPrediction);
+            predictionService.save(predictionNew);
+            character.setPrediction(predictionNew);
+        }
+        return prediction;
     }
 }

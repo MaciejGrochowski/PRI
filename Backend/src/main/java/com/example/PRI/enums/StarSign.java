fefcd1aeb331b8389614532e5b0e9588f994abe7
Zofia Dobrowolska <zofdob@st.amu.edu.pl>
2020-05-21 20:59:33 +0200

    StarSigns update

--- Backend/src/main/java/com/example/PRI/enums/StarSign.java
+++ Backend/src/main/java/com/example/PRI/enums/StarSign.java
@@ -4,51 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
-public enum StarSign { //ToDo to musi być alfabetycznie
-    WYMUND_THE_ANCHORITE("Wymund the Anchorite", "Wymenos", "Sign of Enduring",	"22/12", "7/1"),
-    THE_BIG_CROSS("The Big Cross", "Azurins", "Sign of Clarity", "8/1", "27/1"),
-    THE_LIMNERS_LINE("The Limner's Line", "Verros", "Sign of Precision", "28/1", "15/2"),
-    GNUTHUS_THE_OX("Gnuthus the Ox", "Nuthios", "Sign of Dutiful Service", "16/2", "1/3"),
-    DRAGOMAS_THE_DRAKE("Dragomas the Drake", "Drakonos", "Sign of Courage", "2/3", "21/3"),
-    THE_GLOAMING("The Gloaming", "Tarotes", "Sign of Illusion and Mystery", "22/3", "8/4"),
-    GRUNGNIS_BALDRIC("Grungni's Baldric", "Gileon", "Sign of Martial Pursuits", "9/4", "15/5");
-//    Mammit the Wise 	Mammius 	Sign of Wisdom 	29/4 - 15/5
-//    Mummit the Fool 	Mammios 	Sign of the Indistinct 	16/5 - 1/6
-//    The Two Bullocks 	Hashoor 	Sign of Fertility and Craftsmanship 	2/6 - 21/6
-//    The Dancer 	Adamnos 	Sign of Love and Attraction 	22/6 - 7/7
-//    The Drummer 	Lupios 	Sign of Excess and Hedonism 	8/7 - 27/7
-//    The Piper 	Sangist 	Sign of the Trickster 	28/7 - 15/8
-//    Vobist the Faint 	Vobist 	Sign of Darkness and Uncertainty 	16/8 - 1/9
-//    The Broken Cart 	Kharnos 	Sign of Pride 	2/9 - 21/9
-//    The Greased Goat 	Talios 	Sign of Denied Passions 	22/9 - 8/10
-//    Rhya's Cauldron 	Rionyes 	Sign of Mercy, Death, and Creation 	9/10 - 28/10
-//    Cacklefax the Cockerel 	Kakeros 	Sign of Money and Merchants 	29/10 - 15/11
-//    The Bone Saw (Grimoire) 	Alyoi 	Sign of Skill and Learning 	16/11 - 1/12
-//    The Witchling Star 	Solkios 	Sign of Magic 	2/12 - 21/12
-//
-    //TODO!! Name - Polskie nazwy, znaczenie przetłumaczyć, ShortName do usunięcia, Daty przepisać
-//    Bębniarz – Znak Zabawy i Radości
-//    Dudy – Znak Oszustwa
-//    Dwa Byki – Znak Płodności i Rzemiosła
-//    Głupiec Mummit – Znak Instynktu
-//    Gwiazda Uroku – Znak Magii
-//    Gwiazda Wieczorna – Znak Tajemnicy i Iluzji
-//    Kocioł Rhyi – Znak Łaski, Śmierci i Tworzenia
-//    Lancet – Znak Nauki i Talentu
-//    Mędrzec Marmit – Znak Mądrości
-//    Pas Grungniego – Znak Sprawności Wojennej
-//    Rozbity Wóz – Znak Dumy
-//    Smok Dragomas – Znak Odwagi
-//    Sznur Limnera – Znak Dokładności
-//    Tancerka – Znak Miłości i Pożądania
-//    Tłusty Kozioł – Znak Ukrytej Namiętności
-//    Vobist Ulotny – Znak Ciemności i Niepewności
-//    Wielki Krzyż – Znak Czystości
-//    Wół Gnuthus – Znak Wiernej Służby
-//    Wymund Pustelnik – Znak Wytrzymałości
-//    Złoty Kogut – Znak Kupców i Bogactwa
+public enum StarSign {
 
-
+    BEBNIARZ("Bębniarz", "Lupios", "Znak Zabawy i Radości","8 Nachgeheim","27 Nachgeheim"),
+    DUDY("Dudy", "Sangist", "Znak Oszustwa", "28 Nachgeheim", "15 Erntezeit"),
+    VOBIST_ULOTNY("Vobist Ulotny","Vobist","Znak Ciemności i Niepewności","16 Erntezeit","1 Brauzeit"),
+    ROZBITY_WOZ("Rozbity Wóz","Kharnos","Znak Dumy","2 Brauzeit","21 Brauzeit"),
+    TLUSTY_KOZIOL("Tłusty Kozioł","Talios","Znak Ukrytej Namiętności","22 Brauzeit","8 Kaldezeit"),
+    KOCIOL_RHYI("Kocioł Rhyi","Rionyes","Znak Łaski, Śmierci i Tworzenia","9 Kaldezeit","28 Kaldezeit"),
+    ZLOTY_KOGUT("Złoty Kogut","Kakeros","Znak Kupców i Bogactwa","29 Kaldezeit","15 Ulriczeit"),
+    LANCET("Lancet","Alyoi","Znak Nauki i Talentu","16 Ulriczeit","1 Vorhexen"),
+    GWIAZDA_UROKU("Gwiazda Uroku", "Solkios" , "Znak Magii", "2 Vorhexen", "21 Vorhexen"),
+    WYMUND_PUSTELNIK("Wymund Pustelnik","Wymenos","Znak Wytrzymałości","22 Vorhexen","7 Nachexen"),
+    WIELKI_KRZYZ("Wielki Krzyż","Azurins","Znak Czystości","8 Nachexen","27 Nachexen"),
+    SZNUR_LIMNERA("Sznur Limnera","Verros","Znak Dokładności","28 Nachexen","15 Jahrdrung"),
+    WOL_GNUTHUS("Wół Gnuthus","Nuthios","Znak Wiernej Służby","16 Jahrdrung","1 Pflugzeit"),
+    SMOK_DRAGOMAS("Smok Dragomas","Drakonos","Znak Odwagi","2 Pflugzeit","21 Pflugzeit"),
+    GWIAZDA_WIECZORNA("Gwiazda Wieczorna","Tarotes","Znak Tajemnicy i Iluzji","22 Pflugzeit","8 Sigmarzeit"),
+    PAS_GRUNGNIEGO("Pas Grungniego", "Gileon", "Znak Sprawności Wojennej","9 Sigmarzeit","28 Sigmarzeit"),
+    MEDRZEC_MARMIT("Mędrzec Marmit", "Mammius","Znak Mądrości","29 Sigmarzeit","15 Sommerzeit"),
+    GLUPIEC_MUMMIT("Głupiec Mummit", "Mammios", "Znak Instynktu", "16 Sommerzeit", "1 Vorgeheim"),
+    DWA_BYKI("Dwa byki", "Hashoor", "Znak Płodności i Rzemiosła", "2 Vorgeheim","21 Vorgeheim"),
+    TANCERKA("Tancerka","Adamnos","Znak Miłości i Pożądania","22 Vorgeheim","7 Nachgeheim");
 
 
     private String name;

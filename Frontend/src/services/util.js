8a18768fb761755498fdfb97217d6270648028c1
Zofia Dobrowolska <ikarani24@op.pl>
2020-11-06 23:31:55 +0100

    tooltip text varable

--- Frontend/src/services/util.js
+++ Frontend/src/services/util.js
@@ -7,8 +7,10 @@ export const request = axios.create({
     }
 });
 
-export const localApiUrl = "http://localhost:8080/app";
+ export const localApiUrl = "http://localhost:8080/app";
 // export const localApiUrl = "http://150.254.78.172:443/PRI-0.0.1-SNAPSHOT/app";
+
+//export const localApiUrl = "https://localhost:443/app"
 
 export const apiUrl = localApiUrl;
 
@@ -17,5 +19,7 @@ export const characterUrl = apiUrl + "/characters";
 export const careerUrl = apiUrl + "/careers";
 
 export const placeUrl = apiUrl + "/places";
+
+export const historyUrl = apiUrl + "/histories"
 
 export const generatorUrl = apiUrl + "/generator";

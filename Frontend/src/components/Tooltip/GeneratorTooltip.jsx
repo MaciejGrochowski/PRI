6ac6c27174684c2955b9731391b4ff2810641173
Zofia Dobrowolska <ikarani24@op.pl>
2020-11-24 13:00:33 +0100

    placeOfBeing interactive tooltip

--- Frontend/src/components/Tooltip/GeneratorTooltip.jsx
+++ Frontend/src/components/Tooltip/GeneratorTooltip.jsx
@@ -120,22 +120,43 @@ class GeneratorTooltip extends React.Component {
         return textContent
     }
 
-    //TODO do sprawdzenia!!!!!
+
     actualPlaceText(showRace, showProfession, showSex, showBirthPlace, showReligion){
         let textContent = this.props.content
+        if (showRace && showSex && showProfession && showBirthPlace && showReligion) return textContent += "rasy, płci, profesji, miejsca urodzenia, religii"
+
+        if(!(showRace) && showReligion && showSex && showProfession && showBirthPlace) return textContent +="płci, profesji, miejsca urodzenia, religii"
+        if(!(showSex) && showReligion && showRace && showProfession && showBirthPlace) return textContent +="rasy, profesji, miejsca urodzenia, religii"
+        if(!(showProfession) && showReligion && showRace && showSex && showBirthPlace) return textContent +="rasy, płci, miejsca urodzenia, religii"
+        if(!(showBirthPlace) && showReligion && showRace && showProfession && showSex) return textContent +="rasy, płci, profesji, religii"
+        if(!(showReligion) && showSex && showRace && showProfession && showBirthPlace) return textContent +="rasy, płci, profesji, miejsca urodzenia"
+        if(!(showBirthPlace && showReligion) && showRace && showSex && showProfession) return textContent +="rasy, płci, profesji"
+        if(!(showProfession && showReligion) && showRace && showSex && showBirthPlace) return textContent +="rasy, płci, miejsca urodzenia"
+        if(!(showBirthPlace && showProfession) && showRace && showSex && showReligion) return textContent +="rasy, płci, religii"
+        if(!(showSex && showReligion) && showRace && showProfession && showBirthPlace) return textContent +="rasy, profesji, miejsca urodzenia"
+        if(!(showSex && showBirthPlace) && showRace && showProfession && showReligion) return textContent +="rasy, profesji, religii"
+        if(!(showSex && showProfession) && showRace && showReligion && showBirthPlace) return textContent +="rasy, miejsca urodzenia, religii"
+        if(!(showRace && showReligion) && showBirthPlace && showSex && showProfession) return textContent +="płci, profesji, miejsca urodzenia"
+        if(!(showProfession && showRace) && showReligion && showSex && showBirthPlace) return textContent +="płci, miejsca urodzenia, religii"
+        if(!(showBirthPlace && showRace) && showReligion && showSex && showProfession) return textContent +="płci, profesji, religii"
+        if(!(showProfession && showReligion) && showRace && showSex && showBirthPlace) return textContent +="profesji, miejsca urodzenia, religii"
+
+        if(!(showProfession && showBirthPlace && showReligion) && showRace && showSex) return textContent +="rasy, płci"
+        if(!(showSex && showBirthPlace && showReligion) && showRace && showProfession) return textContent +="rasy, profesji"
+        if(!(showSex && showProfession && showReligion) && showRace && showBirthPlace) return textContent +="rasy, miejsca urodzenia"
+        if(!(showSex && showBirthPlace && showProfession) && showRace && showReligion) return textContent +="rasy, religii"
+        if(!(showRace && showBirthPlace && showReligion) && showSex && showProfession) return textContent +="płci, profesji"
+        if(!(showRace && showProfession && showReligion) && showSex && showBirthPlace) return textContent +="płci, miejsca urodzenia"
+        if(!(showRace && showBirthPlace && showProfession) && showSex && showReligion) return textContent +="płci, religii"
+        if(!(showRace && showSex && showReligion) && showProfession && showBirthPlace) return textContent +="profesji, miejsca urodzenia"
+        if(!(showRace && showBirthPlace && showSex) && showProfession && showReligion) return textContent +="profesji, religii"
+        if(!(showRace && showProfession && showSex) && showBirthPlace && showReligion) return textContent +="miejsca urodzenia, religii"
+
         if (showRace && !(showSex || showProfession || showBirthPlace || showReligion)) return textContent += "rasy"
-        else if(showProfession && !(showRace || showSex || showBirthPlace || showReligion)) return textContent += "profesji"
         else if(showSex && !(showRace || showProfession || showBirthPlace || showReligion)) return textContent += "płci"
+        else if(showProfession && !(showRace || showSex || showBirthPlace || showReligion)) return textContent += "profesji"
         else if(showBirthPlace && !(showRace || showProfession || showSex || showReligion)) return textContent += "miejsca urodzenia"
-        else if(showReligion && !(showRace || showProfession || showSex || showBirthPlace)) return textContent += "relgii"
-        if (showRace && (showSex || showProfession || showBirthPlace || showReligion)) textContent += "rasy, "
-        if (showSex && (showProfession || showBirthPlace || showReligion)) textContent += "płci, "
-        if (showProfession && (showBirthPlace || showReligion)) textContent += "profesji, "
-        if (showBirthPlace && showReligion) textContent += "miejsca urodzenia, "
-        if (showReligion) textContent += "religii"
-        if (showRace && showSex && !(showBirthPlace || showProfession)) textContent += "płci"
-        if ((showProfession && showRace && !(showBirthPlace || showSex)) || (showProfession && showSex && !(showRace || showBirthPlace))) textContent += "profesji"
-        return textContent
+        else if(showReligion && !(showRace || showProfession || showSex || showBirthPlace)) return textContent += "religii"
     }
 
     render() {

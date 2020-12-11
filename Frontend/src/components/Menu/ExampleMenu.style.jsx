cf50bb8bd3e03d7f27d9e2a9a515b98fc6960b3c
MaciejGrochowski <macgro1@st.amu.edu.pl>
2020-12-12 00:45:12 +0100

    add some todos

--- Frontend/src/components/Menu/ExampleMenu.style.jsx
+++ Frontend/src/components/Menu/ExampleMenu.style.jsx
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components';
 
+//ToDo if we generally dont use styled-components, we can delete that
+
 
 export const ItemMenu = styled.span`
     margin-left: 5px;

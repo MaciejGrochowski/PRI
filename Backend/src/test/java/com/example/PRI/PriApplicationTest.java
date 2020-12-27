4309cb7f5ec307f74fcc23b00948f016d0c3355e
Zofia Dobrowolska <ikarani24@op.pl>
2020-12-27 21:29:41 +0100

    Tests fixes

--- Backend/src/test/java/com/example/PRI/PriApplicationTest.java
+++ Backend/src/test/java/com/example/PRI/PriApplicationTest.java
@@ -41,8 +41,25 @@ public class PriApplicationTest {
     @Autowired
     SurnameGenerator surnameGenerator;
 
+    @Autowired
+    NameGenerator nameGenerator;
+
+    @Autowired
+    LivePlaceGenerator livePlaceGenerator;
+
+    @Autowired
+    ApperanceGenerator apperanceGenerator;
+
+
     RaceGenerator raceGenerator = new RaceGenerator();
+    SexGenerator sexGenerator = new SexGenerator();
+    ReligionGenerator religionGenerator = new ReligionGenerator();
+    StatisticsGenerator statisticsGenerator = new StatisticsGenerator();
     CharacterBuilder characterBuilder = new CharacterBuilder();
+    HeightGenerator heightGenerator = new HeightGenerator();
+    WeightGenerator weightGenerator = new WeightGenerator();
+    BirthDateGenerator birthDateGenerator = new BirthDateGenerator();
+
 
         @Test
         public void contextLoads() {
@@ -53,6 +70,7 @@ public class PriApplicationTest {
         characterBuilder.initialize(1234L);
     }
 
+
     @Test
     public void birthPlaceSeedCheck(){
         Character c1 = characterBuilder.buildBirthPlace(characterBirthPlaceGenerator).getCharacter();
@@ -62,22 +80,21 @@ public class PriApplicationTest {
     }
 
 
-        @Test
-        public void raceSeedCheck(){
-            raceGenerator.generateRace(characterBuilder.getCharacter(),characterBuilder.getRandomService(),characterBuilder.getProperties());
-            characterBuilder.buildRace(raceGenerator);
+    @Test
+    public void raceSeedCheck(){
+        raceGenerator.generateRace(characterBuilder.getCharacter(),characterBuilder.getRandomService(),characterBuilder.getProperties());
+        characterBuilder.buildRace(raceGenerator);
 
-            assertEquals(Race.DWARF,characterBuilder.getCharacter().getRace());
-        }
+        assertEquals(Race.DWARF,characterBuilder.getCharacter().getRace());
+    }
+
 
     @Test
     public void sexSeedCheck(){
-        SexGenerator sexGenerator = new SexGenerator();
-        sexGenerator.generateSex(characterBuilder.getCharacter(),characterBuilder.getRandomService(),characterBuilder.getProperties());
-        characterBuilder.buildSex(sexGenerator);
+        Character c1 = characterBuilder.buildRace(raceGenerator).buildSex(sexGenerator).getCharacter();
+        Character c2 = characterBuilder.buildRace(raceGenerator).buildSex(sexGenerator).getCharacter();
 
-        assertEquals(Sex.FEMALE,characterBuilder.getCharacter().getSex());
-
+        assertEquals(c1,c2);
     }
 
     @Test
@@ -88,19 +105,35 @@ public class PriApplicationTest {
         assertEquals(c1,c2);
     }
 
+    @Test
+    public void nameSeedCheck(){
+        Character c1 = characterBuilder.buildRace(raceGenerator).buildSex(sexGenerator).buildName(nameGenerator).getCharacter();
+        Character c2 = characterBuilder.buildRace(raceGenerator).buildSex(sexGenerator).buildName(nameGenerator).getCharacter();
+
+        assertEquals(c1,c2);
+    }
+
 
     @Test
     public void baseStatsSeedCheck(){
-        StatisticsGenerator statisticsGenerator = new StatisticsGenerator();
         Character c1 = characterBuilder.buildBaseStats(statisticsGenerator).getCharacter();
         Character c2 = characterBuilder.buildBaseStats(statisticsGenerator).getCharacter();
 
         assertEquals(c1,c2);
     }
 
+
+    @Test
+    public void birthDateSeedCheck(){
+        Character c1 = characterBuilder.buildRace(raceGenerator).buildBirthDate(birthDateGenerator).getCharacter();
+        Character c2 = characterBuilder.buildRace(raceGenerator).buildBirthDate(birthDateGenerator).getCharacter();
+
+        assertEquals(c1,c2);
+    }
+
+
     @Test
     public void weightSeedCheck(){
-        WeightGenerator weightGenerator = new WeightGenerator();
         Character c1 = characterBuilder.buildRace(raceGenerator).buildWeight(weightGenerator).getCharacter();
         Character c2 = characterBuilder.buildRace(raceGenerator).buildWeight(weightGenerator).getCharacter();
 
@@ -110,7 +143,6 @@ public class PriApplicationTest {
 
     @Test
     public void heightSeedCheck(){
-        HeightGenerator heightGenerator = new HeightGenerator();
         Character c1 = characterBuilder.buildRace(raceGenerator).buildHeight(heightGenerator).getCharacter();
         Character c2 = characterBuilder.buildRace(raceGenerator).buildHeight(heightGenerator).getCharacter();
 
@@ -129,24 +161,13 @@ public class PriApplicationTest {
 
     @Test
     public void hairColorSeedCheck(){
-        RaceGenerator raceGenerator = new RaceGenerator();
         Character c1 = characterBuilder.buildRace(raceGenerator).buildHairColor(hairColorGenerator).getCharacter();
         Character c2 = characterBuilder.buildRace(raceGenerator).buildHairColor(hairColorGenerator).getCharacter();
 
         assertEquals(c1,c2);
     }
 
-    @Test
-    public void birthDateSeedCheck(){
-        BirthDateGenerator birthDateGenerator = new BirthDateGenerator();
-        Character c1 = characterBuilder.buildRace(raceGenerator).buildBirthDate(birthDateGenerator).getCharacter();
-        Character c2 = characterBuilder.buildRace(raceGenerator).buildBirthDate(birthDateGenerator).getCharacter();
 
-        assertEquals(c1,c2);
-    }
-
-
-    //TODO to samo co wyżej
     @Test
     public void emotionSeedCheck(){
         Character c1 = characterBuilder.buildEmotions(emotionGenerator).getCharacter();
@@ -165,12 +186,44 @@ public class PriApplicationTest {
 
 
     @Test
-    public void CareerSeedCheck(){
-        StatisticsGenerator statisticsGenerator = new StatisticsGenerator();
+    public void careerSeedCheck(){
         Character c1 = characterBuilder.buildBaseStats(statisticsGenerator).buildCareers(careerGenerator).getCharacter();
         Character c2 = characterBuilder.buildBaseStats(statisticsGenerator).buildCareers(careerGenerator).getCharacter();
 
         assertEquals(c1,c2);
     }
 
+
+    @Test
+    public void religionSeedCheck(){
+        Character c1 = characterBuilder.buildRace(raceGenerator).buildBirthPlace(characterBirthPlaceGenerator).buildBaseStats(statisticsGenerator).
+                buildCareers(careerGenerator).buildSex(sexGenerator).buildReligion(religionGenerator).getCharacter();
+        Character c2 = characterBuilder.buildRace(raceGenerator).buildBirthPlace(characterBirthPlaceGenerator).buildBaseStats(statisticsGenerator).
+                buildCareers(careerGenerator).buildSex(sexGenerator).buildReligion(religionGenerator).getCharacter();
+
+        assertEquals(c1,c2);
+    }
+
+    
+    @Test
+    public void livePlaceSeedCheck(){
+        Character c1 = characterBuilder.buildBirthPlace(characterBirthPlaceGenerator).buildBaseStats(statisticsGenerator).buildCareers(careerGenerator).
+                buildLivePlace(livePlaceGenerator).buildCareers(careerGenerator).getCharacter();
+        Character c2 = characterBuilder.buildBirthPlace(characterBirthPlaceGenerator).buildBaseStats(statisticsGenerator).buildCareers(careerGenerator).
+                buildLivePlace(livePlaceGenerator).buildCareers(careerGenerator).getCharacter();
+
+        assertEquals(c1,c2);
+    }
+
+    @Test
+    public void apperanceSeedCheck(){
+        Character c1 = characterBuilder.buildRace(raceGenerator).buildBirthPlace(characterBirthPlaceGenerator).
+                buildBaseStats(statisticsGenerator).buildCareers(careerGenerator).buildSex(sexGenerator).buildReligion(religionGenerator).
+                buildHeight(heightGenerator).buildWeight(weightGenerator).buildApperances(apperanceGenerator).getCharacter();
+        Character c2 = characterBuilder.buildRace(raceGenerator).buildBirthPlace(characterBirthPlaceGenerator).
+                buildBaseStats(statisticsGenerator).buildCareers(careerGenerator).buildSex(sexGenerator).buildReligion(religionGenerator).
+                buildHeight(heightGenerator).buildWeight(weightGenerator).buildApperances(apperanceGenerator).getCharacter();
+
+        assertEquals(c1,c2);
+    }
 }

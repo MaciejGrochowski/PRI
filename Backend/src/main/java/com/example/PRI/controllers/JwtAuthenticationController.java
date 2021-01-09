7909141c0df71d96679bc701ab2b561156d07336
MaciejGrochowski <Maciej133@gmail.com>
2021-01-09 02:20:21 +0100

    debug logout

--- Backend/src/main/java/com/example/PRI/controllers/JwtAuthenticationController.java
+++ Backend/src/main/java/com/example/PRI/controllers/JwtAuthenticationController.java
@@ -20,9 +20,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -73,8 +71,8 @@ public class JwtAuthenticationController {
         }
     }
 
-    @Post("/logout-user")
-    //@RequestMapping(value = "/logout-user", method = RequestMethod.POST)
+//    @Post("/logout-user")
+    @RequestMapping(value = "/logout-user", method = RequestMethod.POST)
     private void logout(Authentication auth){
      userOfAppService.logoutUser(auth);
     }

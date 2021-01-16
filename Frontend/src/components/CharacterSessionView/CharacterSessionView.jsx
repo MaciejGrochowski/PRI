10c33b2355b3833fcfcb1444d9f5a521bd8c3a5d
Zofia Dobrowolska <ikarani24@op.pl>
2021-01-16 01:41:22 +0100

    sessions details styling #11 - statistics

--- Frontend/src/components/CharacterSessionView/CharacterSessionView.jsx
+++ Frontend/src/components/CharacterSessionView/CharacterSessionView.jsx
@@ -3,85 +3,96 @@ import {Link} from "react-router-dom";
 import {fronendUrls} from "../../commons/urls";
 
 
-
 class CharacterSessionView extends React.Component {
 
-    render(){
+    render() {
         const {character, isMG, onDeleteCharacter, onChangeVisibilityClick} = this.props;
 
         return (
             <div className="full-character-box">
                 {/*ToDo refactor (as other columnConfigs)*/}
 
-                <div className="character-name">{character.name || character.surname ? character.name + (character.surname ? " " + character.surname: "") : "Nieznana"}</div>
+                <div
+                    className="character-name">{character.name || character.surname ? character.name + (character.surname ? " " + character.surname : "") : "Nieznana"}</div>
 
                 <div className="character-information">
                     <div className="flex-container-session-2">
                         <div className="short-info">
 
                             <div className="flex-container-session-2">
-                {character.race && <div className="inside-character-1">Rasa: </div>}
-                {character.race && <div className="inside-character-2">{character.race}</div>}
+                                {character.race && <div className="inside-character-1">Rasa: </div>}
+                                {character.race && <div className="inside-character-2">{character.race}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.sex && <div className="inside-character-1">Płeć: </div>}
-                {character.sex && <div className="inside-character-2">{character.sex}</div>}
+                                {character.sex && <div className="inside-character-1">Płeć: </div>}
+                                {character.sex && <div className="inside-character-2">{character.sex}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.currentCareer && <div className="inside-character-1">Profesja: </div>}
-                {character.currentCareer && <div className="inside-character-2">{character.currentCareer}</div>}
+                                {character.currentCareer && <div className="inside-character-1">Profesja: </div>}
+                                {character.currentCareer &&
+                                <div className="inside-character-2">{character.currentCareer}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.livePlace && <div className="inside-character-1">Miejsce pobytu: </div>}
-                {character.livePlace && <div className="inside-character-2">{character.livePlace}</div>}
+                                {character.livePlace && <div className="inside-character-1">Miejsce pobytu: </div>}
+                                {character.livePlace && <div className="inside-character-2">{character.livePlace}</div>}
 
                             </div>
                             <div className="flex-container-session-2">
-                {character.birthPlace && <div className="inside-character-1">Miejsce urodzenia: </div>}
-                {character.birthPlace && <div className="inside-character-2">{character.birthPlace}</div>}
+                                {character.birthPlace && <div className="inside-character-1">Miejsce urodzenia: </div>}
+                                {character.birthPlace &&
+                                <div className="inside-character-2">{character.birthPlace}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.dayOfBirth && <div className="inside-character-1">Dzień urodzenia: </div>}
-                {character.dayOfBirth && <div className="inside-character-2">{character.dayOfBirth}</div>}
+                                {character.dayOfBirth && <div className="inside-character-1">Dzień urodzenia: </div>}
+                                {character.dayOfBirth &&
+                                <div className="inside-character-2">{character.dayOfBirth}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.monthOfBirth && <div className="inside-character-1">Miesiąc urodzenia: </div>}
-                {character.monthOfBirth && <div className="inside-character-2">{character.monthOfBirth}</div>}
+                                {character.monthOfBirth &&
+                                <div className="inside-character-1">Miesiąc urodzenia: </div>}
+                                {character.monthOfBirth &&
+                                <div className="inside-character-2">{character.monthOfBirth}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.yearOfBird && <div className="inside-character-1">Rok urodzenia: </div>}
-                {character.yearOfBird && <div className="inside-character-2">{character.yearOfBird}</div>}
+                                {character.yearOfBirth && <div className="inside-character-1">Rok urodzenia: </div>}
+                                {character.yearOfBirth &&
+                                <div className="inside-character-2">{character.yearOfBirth}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.religion && <div className="inside-character-1">Religia: </div>}
-                {character.religion && <div className="inside-character-2">{character.religion}</div>}
+                                {character.starSign && <div className="inside-character-1">Znak gwiezdny: </div>}
+                                {character.starSign && <div className="inside-character-2">{character.starSign}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.height && <div className="inside-character-1">Wzrost: </div>}
-                {character.height && <div className="inside-character-2">{character.height}</div>}
+                                {character.religion && <div className="inside-character-1">Religia: </div>}
+                                {character.religion && <div className="inside-character-2">{character.religion}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.weight && <div className="inside-character-1">Waga: </div>}
-                {character.weight && <div className="inside-character-2">{character.weight}</div>}
+                                {character.height && <div className="inside-character-1">Wzrost: </div>}
+                                {character.height && <div className="inside-character-2">{character.height}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.eyeColor && <div className="inside-character-1">Kolor oczu: </div>}
-                {character.eyeColor && <div className="inside-character-2">{character.eyeColor}</div>}
+                                {character.weight && <div className="inside-character-1">Waga: </div>}
+                                {character.weight && <div className="inside-character-2">{character.weight}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
-                {character.hairColor && <div className="inside-character-1">Kolor włosów: </div>}
-                {character.hairColor && <div className="inside-character-2">{character.hairColor}</div>}
+                                {character.eyeColor && <div className="inside-character-1">Kolor oczu: </div>}
+                                {character.eyeColor && <div className="inside-character-2">{character.eyeColor}</div>}
+                            </div>
+
+                            <div className="flex-container-session-2">
+                                {character.hairColor && <div className="inside-character-1">Kolor włosów: </div>}
+                                {character.hairColor && <div className="inside-character-2">{character.hairColor}</div>}
                             </div>
 
                             <div className="flex-container-session-2">
@@ -89,59 +100,109 @@ class CharacterSessionView extends React.Component {
                                 {character.personality && <div className="inside-character-4">{character.personality}</div>}
                             </div>
 
-
                             <div className="flex-container-session-2">
                                 {character.apperance && <div className="inside-character-3">Cechy wyglądu</div>}
-                                {character.personality && <div className="inside-character-4">{character.apperance}</div>}
+                                {character.apperance && <div className="inside-character-4">{character.apperance}</div>}
                             </div>
+
+                            <div className="flex-container-session-2">
+                                {character.previousCareers && <div className="inside-character-3">Poprzednie profesje</div>}
+                                {character.previousCareers && <div className="inside-character-4">{character.previousCareers}</div>}
+                            </div>
+
+                            <div className="flex-container-session-2">
+                                {character.dominatingEmotions && <div className="inside-character-3">Dominujące emocje</div>}
+                                {character.dominatingEmotions && <div className="inside-character-4">{character.dominatingEmotions}</div>}
+                            </div>
+
+                            <div className="flex-container-session-2">
+                                {character.skills && <div className="inside-character-3">Umiejętności</div>}
+                                {character.skills && <div className="inside-character-4">{character.skills}</div>}
+                            </div>
+
+                            <div className="flex-container-session-2">
+                                {character.talents && <div className="inside-character-3">Zdolności</div>}
+                                {character.talents && <div className="inside-character-4">{character.talents}</div>}
+                            </div>
+
+                            <div className="flex-container-session-2">
+                                {character.prediction && <div className="inside-character-3">Przepowiednia</div>}
+                                {character.prediction && <div className="inside-character-4">{character.prediction}</div>}
+                            </div>
+
+
+                            <div className="flex-container-session-2">
+                                <div className="character-statistic">
+                                    {character.endWeaponSkills && <div className="inside-character-5">WW</div>}
+                                    {character.endWeaponSkills && <div className="inside-character-5">{character.endWeaponSkills}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endBallisticSkills && <div className="inside-character-5">US</div>}
+                                    {character.endBallisticSkills && <div className="inside-character-5">{character.endBallisticSkills}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endStrength && <div className="inside-character-5">K</div>}
+                                    {character.endStrength && <div className="inside-character-5">{character.endStrength}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endToughness && <div className="inside-character-5">ODP</div>}
+                                    {character.endToughness && <div className="inside-character-5">{character.endToughness}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endAgility && <div className="inside-character-5">ZR</div>}
+                                    {character.endAgility && <div className="inside-character-5">{character.endAgility}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endIntelligence && <div className="inside-character-5">INT</div>}
+                                    {character.endIntelligence && <div className="inside-character-5">{character.endIntelligence}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endWillPower && <div className="inside-character-5">SW</div>}
+                                    {character.endWillPower && <div className="inside-character-5">{character.endWillPower}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endFellowship && <div className="inside-character-5">OGD</div>}
+                                    {character.endFellowship && <div className="inside-character-5">{character.endFellowship}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endAttacks && <div className="inside-character-5">A</div>}
+                                    {character.endAttacks && <div className="inside-character-5">{character.endAttacks}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endWounds && <div className="inside-character-5">ŻYW</div>}
+                                    {character.endWounds && <div className="inside-character-5">{character.endWounds}</div>}
+                                </div>
+
+
+                                <div className="character-statistic">
+                                    {character.endMagic && <div className="inside-character-5">MAG</div>}
+                                    {character.endMagic && <div className="inside-character-5">{character.endMagic}</div>}
+                                </div>
+
+                                <div className="character-statistic">
+                                    {character.endMovement && <div className="inside-character-5">SZ</div>}
+                                    {character.endMovement && <div className="inside-character-5">{character.endMovement}</div>}
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
 
-
+                    <div>
+                        {isMG && <Link to={fronendUrls.characterDetails + "/" + character.id}>Więcej</Link>}
+                        {isMG && <button onClick={() => onDeleteCharacter(character.id)}>Usuń postać</button>}
+                        {isMG &&
+                        <button onClick={() => onChangeVisibilityClick(character.id)}>Zmień widoczność cech</button>}
                     </div>
-
-
-                {character.apperance && <div>Cechy wyglądu: {character.apperance}</div>}
-
-                {character.previousCareers && <div>Poprzednie profesje: {character.previousCareers}</div>}
-
-                {character.dominatingEmotions && <div>Dominujące emocje: {character.dominatingEmotions}</div>}
-
-                {character.skills && <div>Umiejętności: {character.skills}</div>}
-
-                {character.talents && <div>Zdolności: {character.talents}</div>}
-
-                {character.prediction && <div>Przepowiednia: {character.prediction}</div>}
-
-                {character.starSign && <div>Znak gwiezdny: {character.starSign}</div>}
-
-                {character.endWeaponSkills && <div>WW: {character.endWeaponSkills}</div>}
-
-                {character.endBallisticSkills && <div>US: {character.endBallisticSkills}</div>}
-
-                {character.endStrength && <div>K: {character.endStrength}</div>}
-
-                {character.endToughness && <div>ODP: {character.endToughness}</div>}
-
-                {character.endAgility && <div>ZR: {character.endAgility}</div>}
-
-                {character.endIntelligence && <div>INT: {character.endIntelligence}</div>}
-
-                {character.endWillPower && <div>SW: {character.endWillPower}</div>}
-
-                {character.endFellowship && <div>OGD: {character.endFellowship}</div>}
-
-                {character.endAttacks && <div>A: {character.endAttacks}</div>}
-
-                {character.endWounds && <div>ŻYW: {character.endWounds}</div>}
-
-                {character.endMagic && <div>MAG: {character.endMagic}</div>}
-
-                {character.endMovement && <div>SZ: {character.endMovement}</div>}
-
-
-                {isMG && <Link to={fronendUrls.characterDetails + "/" + character.id}>Więcej</Link>}
-                {isMG && <button onClick={() => onDeleteCharacter(character.id)}>Usuń postać</button>}
-                {isMG && <button onClick={() => onChangeVisibilityClick(character.id)}>Zmień widoczność cech</button>}
                 </div>
 
 

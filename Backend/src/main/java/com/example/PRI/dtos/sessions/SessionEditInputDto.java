d7ecb63a35c9522a5f371344eddb9a394017454b
MaciejGrochowski <Maciej133@gmail.com>
2021-01-17 02:33:50 +0100

    x

--- Backend/src/main/java/com/example/PRI/dtos/sessions/SessionEditInputDto.java
+++ Backend/src/main/java/com/example/PRI/dtos/sessions/SessionEditInputDto.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SessionEditInputDto {
     String hashcode;
+    @NotBlank(message = "NO_SESSION_NAME")
     String name;
     String description;
 }

db6cf4e3cf606db2bcbd3988ff4308bc6803b60d
MaciejGrochowski <Maciej133@gmail.com>
2021-01-19 00:51:31 +0100

    fix mistake

--- Frontend/src/pages/MainPage.jsx
+++ Frontend/src/pages/MainPage.jsx
@@ -38,7 +38,7 @@ class MainPage extends React.Component {
 
 
                 <div className="subsection-container warm-grey-section">
-                    <div className="section-title">Uważasz, że to świetny pomysł i chciałbyś jakoś pomóc?</div>
+                    <div className="section-title">Uważasz, że to świetny pomysł i chcesz jakoś pomóc?</div>
                     <div className="welcome-text">
                         <div> Wspaniale! Jesteśmy Tobą zainteresowani! Aplikacja JaNieTakiOrk wciąż potrzebuje rozwoju i sporej ilości pracy. Jak możesz pomóc? </div>
                         <div className="margin-bottom"><div className="bold"> {arrow} <Link to={fronendUrls.registerPage}>Załóż konto!</Link></div> Twórz postacie i historie z rozegranych przez siebie sesji. Dodaj temu miejscu życia!</div>
@@ -51,7 +51,7 @@ class MainPage extends React.Component {
 
                         <div className="margin-bottom"> <div className="bold">{arrow} Pomóż nam ulepszyć generator.</div> Nasz generator korzysta z ogromnej ilości różnych danych i zauważonych przez nas zależności w świecie Warhammera. Jeśli jesteś doświadczonym graczem (lub MG), niestraszne Ci rozważania na temat prawdopodobieństwa w świecie gry i doskonale wiesz jak powinna rosnąć szansa na posiadanie brody w przypadku wygenerowania mężczyzny krasnoluda, to zapraszamy!</div>
 
-                        <div className="margin-bottom"> <div className="bold">{arrow} Zostań programistą.</div>  Aplikacja JaNieTakiOrk powstała w Javascript (z użyciem frameworku React) oraz Java(z użyciem frameworku Spring). Jeśli chcesz pomóc w rozwoju opensourcowej aplikacji, skontaktuj się z nami lub samodzielnie dokonaj zmian i wyślij nam pull requesta na repozytorium: <a href="https://github.com/MaciejGrochowski/PRI">Github</a> (Opcja dla bardziej zaawansowanych, którzy wiedzą co robią)</div>
+                        <div className="margin-bottom"> <div className="bold">{arrow} Programuj z nami.</div>  Aplikacja JaNieTakiOrk powstała w Javascript (z użyciem frameworku React) oraz Java(z użyciem frameworku Spring). Jeśli chcesz pomóc w rozwoju opensourcowej aplikacji, skontaktuj się z nami lub samodzielnie dokonaj zmian i wyślij nam pull requesta na repozytorium: <a href="https://github.com/MaciejGrochowski/PRI">Github</a> (Opcja dla bardziej zaawansowanych, którzy wiedzą co robią)</div>
 
                         </div>
                     </div>

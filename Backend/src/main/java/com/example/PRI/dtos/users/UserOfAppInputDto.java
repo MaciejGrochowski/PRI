9784dd6f1a631f2b7b78f8d402d5aacdbfabf40b
Zofia Dobrowolska <ikarani24@op.pl>
2021-01-26 12:31:32 +0100

    password fix

--- Backend/src/main/java/com/example/PRI/dtos/users/UserOfAppInputDto.java
+++ Backend/src/main/java/com/example/PRI/dtos/users/UserOfAppInputDto.java
@@ -14,13 +14,13 @@ public class UserOfAppInputDto {
     @Pattern(regexp = "^(([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?))&(^.{2,200}$)$", message ="NOT_EMAIL_REGEX" )
     String mail;
 
-    @Pattern(regexp = "^(.{2,21})&(([a-zA-Z]*[\\|!@#$%^&*()\\-_\\\\\\/><.,=+~`'\"{}\\[\\]:;\\^]*)*)&(([a-zA-Z]*\\d*)*)&(([a-z]*[\\|!@#$%^&*()\\-_\\\\\\/><.,=+~`'\"{}\\[\\]:;\\^]*\\d*)*)$", message = "WRONG_PASSWORD_FORM")
+    @Pattern(regexp = "^(.{3,64})&(([a-zA-Z]*[\\|!@#$%^&*()\\-_\\\\\\/><.,=+~`'\"{}\\[\\]:;\\^]*)*)&(([a-zA-Z]*\\d*)*)&(([a-z]*[\\|!@#$%^&*()\\-_\\\\\\/><.,=+~`'\"{}\\[\\]:;\\^]*\\d*)*)$", message = "WRONG_PASSWORD_FORM")
     String password;
 
     @Pattern(regexp = "^.{3,20}$", message = "WRONG_USERNAME_LENGTH")
     String username;
 
-    @Pattern(regexp = "^(.{2,21})&(([a-zA-Z]*[\\|!@#$%^&*()\\-_\\\\\\/><.,=+~`'\"{}\\[\\]:;\\^]*)*)&(([a-zA-Z]*\\d*)*)&(([a-z]*[\\|!@#$%^&*()\\-_\\\\\\/><.,=+~`'\"{}\\[\\]:;\\^]*\\d*)*)$", message = "WRONG_PASSWORD_FORM")
+    @Pattern(regexp = "^(.{3,64})&(([a-zA-Z]*[\\|!@#$%^&*()\\-_\\\\\\/><.,=+~`'\"{}\\[\\]:;\\^]*)*)&(([a-zA-Z]*\\d*)*)&(([a-z]*[\\|!@#$%^&*()\\-_\\\\\\/><.,=+~`'\"{}\\[\\]:;\\^]*\\d*)*)$", message = "WRONG_PASSWORD_FORM")
     String confirmPassword;
 
     @Pattern(regexp = "https://www.facebook.com/.*", message = "WRONG_FACEBOOK_LINK")

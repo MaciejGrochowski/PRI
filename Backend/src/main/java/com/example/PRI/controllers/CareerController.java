a0783ce4ce64fab597b67348e9b3db48a7cafc32
MaciejGrochowski <Maciej133@gmail.com>
2021-09-02 15:42:54 +0200

    test3

--- Backend/src/main/java/com/example/PRI/controllers/CareerController.java
+++ Backend/src/main/java/com/example/PRI/controllers/CareerController.java
@@ -18,7 +18,7 @@ public class CareerController {
     CareerService careerService;
 
     public void test(){
-        System.out.println("r34erer43");
+        System.out.println("r34erdsfdsffder43");
     }
 
     @Get("/names/all")

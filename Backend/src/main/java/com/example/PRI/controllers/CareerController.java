f8a1f9c8755a53721cba659671b6c240f8d0d12f
MaciejGrochowski <Maciej133@gmail.com>
2021-09-02 15:03:06 +0200

    t1

--- Backend/src/main/java/com/example/PRI/controllers/CareerController.java
+++ Backend/src/main/java/com/example/PRI/controllers/CareerController.java
@@ -17,6 +17,10 @@ public class CareerController {
     @Autowired
     CareerService careerService;
 
+    public void test(){
+        System.out.println("Test323442523");
+    }
+
     @Get("/names/all")
     public List<String> getAlCareerNames() {
         return careerService.getAllNames();
